ea0c3bffbb1817eef6b50fc75290c2b32d7b9345
Smiech <masternike1207@gmail.com>
2025-12-07 18:06:07 +0100

    RockProjectile is now in the hitbox, have to fix jittering

--- src/main/java/net/smiech/cryptidologica/entity/client/customProjectiles/RockProjectileRenderer.java
+++ src/main/java/net/smiech/cryptidologica/entity/client/customProjectiles/RockProjectileRenderer.java
@@ -3,15 +3,12 @@ package net.smiech.cryptidologica.entity.client.customProjectiles;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import net.minecraft.client.model.LlamaSpitModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
-import net.minecraft.world.entity.projectile.LlamaSpit;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.smiech.cryptidologica.CryptidOlogica;
@@ -27,20 +24,22 @@ public class RockProjectileRenderer extends EntityRenderer<RockProjectileEntity>
         this.model = new RockProjectileModel<>(pContext.bakeLayer(RockProjectileModel.ROCK_PROJECTILE));
     }
 
+
+
+    public void render(RockProjectileEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
+        pPoseStack.pushPose();
+
+        pPoseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.yRotO, pEntity.getYRot()) - 90.0F));
+        pPoseStack.mulPose(Axis.ZP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.xRotO, pEntity.getXRot())));
+        pPoseStack.translate(0.0F, -1F, 0.0F);
+        this.model.setupAnim(pEntity, pPartialTicks, 0.0F, -0.1F, 0.0F, 0.0F);
+        VertexConsumer textureBuffer = pBuffer.getBuffer(this.model.renderType(ROCK_PROJECTILE_LOCATION));
+        this.model.renderToBuffer(pPoseStack, textureBuffer, pPackedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+        pPoseStack.popPose();
+        super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
+    }
     @Override
     public ResourceLocation getTextureLocation(RockProjectileEntity rockProjectileEntity) {
         return ROCK_PROJECTILE_LOCATION;
     }
-
-    public void render(RockProjectileEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
-        pPoseStack.pushPose();
-        pPoseStack.translate(0.0F, 0.15F, 0.0F);
-        pPoseStack.mulPose(Axis.YP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.yRotO, pEntity.getYRot()) - 90.0F));
-        pPoseStack.mulPose(Axis.ZP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.xRotO, pEntity.getXRot())));
-        VertexConsumer $$6 = pBuffer.getBuffer(this.model.renderType(ROCK_PROJECTILE_LOCATION));
-        this.model.renderToBuffer(pPoseStack, $$6, pPackedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
-        pPoseStack.popPose();
-        super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
-    }
-
 }
